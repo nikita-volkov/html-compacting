cf618fb620f3fca7a3a58a5b9729e8bd70a03fe4
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-02-19 19:31:22 +0300

    Add tests

---
+++ tests/Main.hs
@@ -0,0 +1,27 @@
+module Main where
+
+import Prelude
+import Test.QuickCheck.Instances
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+import Data.XML.Types
+import qualified HTML.Compacting as A
+import qualified Control.Foldl as B
+
+
+main =
+  defaultMain $
+  testGroup "All tests" $
+  [
+    testCase "" $
+    assertEqual "" [element "p" [" a b"]] (A.process [element "p" ["  a ", "", " b"]])
+  ]
+
+instance IsString Node where
+  fromString =
+    NodeContent . ContentText . fromString
+
+element :: Text -> [Node] -> Node
+element name nodes =
+  NodeElement (Element (Name name Nothing Nothing) [] nodes)
